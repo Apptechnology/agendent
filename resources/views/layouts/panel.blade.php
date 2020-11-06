7bd5959275640a00ba34861c4b5efe743309f00d
Felipe Martin <felipem@MacBook-Pro-de-Felipe.local>
2020-11-06 14:13:45 -0600

    Specialty CRUD

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -108,7 +108,7 @@
     <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
-        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Dashboard</a>
+        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Panel de administración</a>
         <!-- Form -->
         <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
           <div class="form-group mb-0">
@@ -163,8 +163,7 @@
                                        onclick="event.preventDefault();
 
                                                      document.getElementById('logout-form').submit();"> <i class="ni ni-user-run"></i>
-                                        {{ __('Logout') }}
-                                    </a>
+                                    Cerrar Sesión</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
